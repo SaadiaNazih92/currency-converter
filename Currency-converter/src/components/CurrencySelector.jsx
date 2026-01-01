2b5a608ee0b4164f27cd6ca4e481c134cc9b0402
Saadia Nazih <saadia.nazih92@gmail.com>
2026-01-01 06:44:06 +0100

    drobdown menu

--- Currency-converter/src/components/CurrencySelector.jsx
+++ Currency-converter/src/components/CurrencySelector.jsx
@@ -2,4 +2,12 @@ function CurrencySelector({ label, currencies, selectedCurrency, onChange })
 {
     return (
     <div className="flex flex-col w-full mb-4"></div>
+    <label className="mb-2 font-bold text-gray-700">
+        {label}
+      </label>
+      <select
+        value={selectedCurrency}
+        onChange={(e) => onChange(e.target.value)}
+        className="..."
+      ></select>
 }
